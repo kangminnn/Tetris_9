893a3c19d594284726b9af7e56f878779bed1ae2
VeryBigsilver <verybigsilver@gmail.com>
2025-05-30 11:59:53 +0900

    red
    red 구현함

--- Tetris_9/RedBlock.h
+++ Tetris_9/RedBlock.h
@@ -1,7 +1,11 @@
 #pragma once
 #include "Block.h"
 #include "Board.h"
+#include <vector>
 class RedBlock : public Block {
+private:
+    mutable vector<pair<int, int>> delete_red;
+    const int MAX_RED = 10;
 public:
     RedBlock(int shape, int angle, int x, int y);
 
