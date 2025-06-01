9c99e0a8ac7b2b482af174cd5ae56813f36a2853
kangminnn <cube030524@naver.com>
2025-06-01 17:26:06 +0900

    블럭확률 스테이지별

--- Tetris_9/BlockFactory.cpp
+++ Tetris_9/BlockFactory.cpp
@@ -11,18 +11,27 @@
 #include "CyanBlock.h"
 #include "WhiteBlock.h"
 
-
+int blockRate[8][6] = {
+    {25, 0, 0, 0, 0, 0},
+    {25, 50, 0, 0, 0, 0},
+    {25, 50, 60, 0, 0, 0},
+    {20, 35, 45, 65, 0, 0},
+    {20, 35, 45, 65, 75, 0},
+    {20, 35, 45, 65, 75, 80},
+    {20, 40, 50, 70, 80, 87},
+    {23, 46, 56, 70, 80, 85}
+};
 
 unique_ptr<Block> BlockFactory::makeBlock(int& level)
 {
 	int shape = rand() % 7;		//shape에는 0~6의 값이 들어감
     int r = rand() % 100 + 1; // 1 ~ 100 (확률 나누기 위함)
-    if (stage_data[level].abilities.blueBlockAbility && r>0 && r<=20) return make_unique<BlueBlock>(shape, 0, 5, -4);
-    if (stage_data[level].abilities.yellowBlockAbility && r>20 && r<=35) return make_unique<YellowBlock>(shape, 0, 5, -4);
-    if (stage_data[level].abilities.cyanBlockAbility && r > 35 && r <= 45) return make_unique<CyanBlock>(shape, 0, 5, -4);
-    if (stage_data[level].abilities.redBlockAbility && r > 45 && r <= 60) return make_unique<RedBlock>(shape, 0, 5, -4);
-    if (stage_data[level].abilities.purpleBlockAbility && r > 60 && r <= 75) return make_unique<PurpleBlock>(shape, 0, 5, -4);
-    if (stage_data[level].abilities.greenBlockAbility && r > 75 && r <= 82) return make_unique<GreenBlock>(shape, 0, 5, -4);
+    if (r>0 && r <= blockRate[level][0]) return make_unique<BlueBlock>(shape, 0, 5, -4);
+    if (r> blockRate[level][0] && r<= blockRate[level][1]) return make_unique<YellowBlock>(shape, 0, 5, -4);
+    if (r > blockRate[level][1] && r <= blockRate[level][2]) return make_unique<CyanBlock>(shape, 0, 5, -4);
+    if (r > blockRate[level][2] && r <= blockRate[level][3]) return make_unique<RedBlock>(shape, 0, 5, -4);
+    if (r > blockRate[level][3] && r <= blockRate[level][4]) return make_unique<PurpleBlock>(shape, 0, 5, -4);
+    if (r > blockRate[level][4] && r <= blockRate[level][5]) return make_unique<GreenBlock>(shape, 0, 5, -4);
     else return make_unique<WhiteBlock>(shape, 0, 5, -4);
 }
 
